3e65f8ad6bfbf87a585bfd463f109db0dcafe622
gom20 <rhaldud89@gmail.com>
2023-02-10 20:59:23 +0900

    css error resolve

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { usePageTransitionFix } from '@/hooks/usePageTransitionHook';
 import '@/styles/globals.css';
 import { AnimatePresence, motion } from 'framer-motion';
 import { NextPage } from 'next';
@@ -14,7 +15,7 @@ type AppPropsWithLayout = AppProps & {
 
 export default function App({ Component, pageProps, router }: AppPropsWithLayout) {
     const getLayout = Component.getLayout ?? ((page) => page);
-
+    usePageTransitionFix();
     return getLayout(
         <AnimatePresence mode="popLayout" initial={true}>
             <motion.div key={router.pathname} initial={{ opacity: 0, y: '100%', x: 0 }} animate={{ opacity: 1, y: 0, x: 0 }} exit={{ opacity: 0, y: '-100%', x: 0 }} transition={{ duration: 0.5 }}>
